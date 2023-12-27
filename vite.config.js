e0552df01a3afec0802e50c4bb9f2dbb130e9d7d
wr <2119495210@qq.com>
2023-12-27 18:30:23 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+// vite.config.js
 export default {
-    build: {
-      base: '/webgpu-ray-tracing-demo/' // 替换为你的仓库名称
-    }
-  }
+  server: {
+    host: "localhost",
+    port: 8848,
+  },
+  // 设置项目的基础路径，用于GitHub Pages
+  base: "/webgpu-ray-tracing-demo/", // 替换为你的仓库名称
+
+  // 构建输出目录
+  build: {
+    outDir: "dist",
+  },
+};  
